a8b560c778e6bbbff768fb233e2fca7a3fb93450
a <a@as-Virtual-Machine.local>
2026-06-03 23:09:29 -0700

    Introduce persistent state atom for plugin runtime data

--- src/main.cljs
+++ src/main.cljs
@@ -1,6 +1,10 @@
 (ns main)
 
+(defonce state
+  (atom {}))
+
 (defn main
   [plugin]
+  (reset! state {:nvim (.-nvim plugin)})
   (.registerAutocmd plugin "BufEnter" (fn []) (clj->js {:pattern "*.sift"}))
   (.registerCommand plugin "Sift" (fn [])))
