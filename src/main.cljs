6bf122f34f21529826b96677ab06a64325a6c1d2
a <a@as-Virtual-Machine.local>
2026-06-04 00:19:08 -0700

    Introduce register-command helper to wrap plugin command registration

--- src/main.cljs
+++ src/main.cljs
@@ -6,8 +6,8 @@
   (atom {}))
 
 (defn call-function
-  [fname args]
-  (.then (.callFunction (:nvim @state) fname (clj->js args))
+  [function-name options]
+  (.then (.callFunction (:nvim @state) function-name (clj->js options))
          #(js->clj % :keywordize-keys true)))
 
 (def clean
@@ -18,12 +18,16 @@
         split-lines))
 
 (defn sift
-  []
+  [filename]
   (promesa/let [s (call-function "getreg" ["+"])]
     (clean s)))
+
+(defn register-command
+  [plugin command-name handle options]
+  (.registerCommand plugin command-name #(apply handle (js->clj % :keywordize-keys true)) (clj->js options)))
 
 (defn main
   [plugin]
   (reset! state {:nvim (.-nvim plugin)})
   (.registerAutocmd plugin "BufEnter" (fn []) (clj->js {:pattern "*.sift"}))
-  (.registerCommand plugin "Sift" sift))
+  (register-command plugin "Sift" sift {:nargs 1}))
