f172dce3e064d69eb1aca964c22cb50bda71db20
a <a@as-Virtual-Machine.local>
2026-06-03 23:28:03 -0700

    Add call-function helper to wrap Neovim RPC calls with promise handling

--- src/main.cljs
+++ src/main.cljs
@@ -3,8 +3,17 @@
 (defonce state
   (atom {}))
 
+(defn call-function
+  [fname args]
+  (.then (.callFunction (:nvim @state) fname (clj->js args))
+         #(js->clj % :keywordize-keys true)))
+
+(defn sift
+  []
+  (call-function "getreg" ["+"]))
+
 (defn main
   [plugin]
   (reset! state {:nvim (.-nvim plugin)})
   (.registerAutocmd plugin "BufEnter" (fn []) (clj->js {:pattern "*.sift"}))
-  (.registerCommand plugin "Sift" (fn [])))
+  (.registerCommand plugin "Sift" sift))
