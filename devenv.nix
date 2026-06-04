f83671169747f799c66c1c80d4c7c2d635fcde9f
a <a@as-Virtual-Machine.local>
2026-06-03 22:57:59 -0700

    Add watch script to stream Neovim terminal output from node.log

--- devenv.nix
+++ devenv.nix
@@ -33,6 +33,9 @@
   scripts.run.exec = ''
     nvim demo.sift
   '';
+  scripts.watch.exec = ''
+    nvim +star "+te tail -F node.log -n +1"
+  '';
 
   # https://devenv.sh/basics/
   enterShell = ''
@@ -40,6 +43,8 @@
     git --version # Use packages
     npm i
     export PATH="$DEVENV_ROOT/node_modules/.bin:$PATH"
+    export NVIM_NODE_LOG_FILE="$DEVENV_ROOT/node.log"
+    export NVIM_NODE_LOG_LEVEL=info
   '';
 
   # https://devenv.sh/tasks/
