59b4104e4ff28f1e293c5c366af20bd89ba14e98
a <a@as-Virtual-Machine.local>
2026-06-07 01:27:18 -0700

    Add ghcid to development environment and configure watch script to use it

--- devenv.nix
+++ devenv.nix
@@ -9,6 +9,7 @@
 
   # https://devenv.sh/packages/
   packages = [
+    pkgs.ghcid
     pkgs.git
     pkgs.gitleaks
     pkgs.nil
@@ -34,8 +35,21 @@
     cd "$DEVENV_ROOT/cljs" && shadow-cljs release main
   '';
   scripts.run.exec = "nvim demo.sift";
-  scripts.watch.exec = ''
+  scripts.log.exec = ''
     nvim +star "+te tail -F node.log -n +1"
+  '';
+  # ':set -Wprepositive-qualified-module' command works around a ghcid crash related to the `-Wprepositive-qualified-module` warning.
+  # The warning can be triggered by GHCi's internal startup process, causing a crash if enabled from the start.
+  # The fix is to disable the warning during initial GHCi loading in a .ghci file with `:set -Wno-prepositive-qualified-module`
+  # and then use this ghcid command to re-enable it after ghcid has successfully started.
+  # The trade-off is that the initial module load is not checked for this specific warning.
+  scripts.watch.exec = ''
+    cd "$DEVENV_ROOT/hs" && ghcid -a \
+    -c 'stack ghci --ghci-options "-ghci-script load.ghci" --no-load ' \
+    --no-height-limit \
+    -r \
+    -s ':set -Wprepositive-qualified-module' \
+    -W
   '';
 
   # https://devenv.sh/basics/
