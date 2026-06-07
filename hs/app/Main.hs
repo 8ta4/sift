60280c17889e28b104caa1ab7c3a1bc7128dd049
a <a@as-Virtual-Machine.local>
2026-06-07 15:46:21 -0700

    Add browser‑specific manifest generation for Firefox and Chrome

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -1,27 +1,39 @@
 module Main (main) where
 
-import Data.Aeson (Value (Object), encode, object, (.=))
+import Data.Aeson (Object, Value (Object), encode, object, (.=))
 import Data.Aeson.KeyMap qualified as KeyMap
 import Relude
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath (takeDirectory)
 
 main :: IO ()
-main = writeManifest "../cljs/public/manifest.json"
+main = do
+  writeManifest "../cljs/public/manifest.json" firefox
+  writeManifest "../cljs/release/manifest.json" chrome
 
-writeManifest :: FilePath -> IO ()
-writeManifest path = do
+writeManifest :: FilePath -> Object -> IO ()
+writeManifest path config = do
   createDirectoryIfMissing True $ takeDirectory path
-  writeFileLBS path
-    $ encode
-    $ Object
-    $ KeyMap.fromList
-      [ "background"
-          .= object
-            [ "scripts" .= ["js/background.js" :: Text],
-              "type" .= ("module" :: Text)
-            ],
-        "manifest_version" .= (3 :: Int),
-        "name" .= ("sift" :: Text),
-        "version" .= ("0.1.0" :: Text)
-      ]
+  writeFileLBS path $ encode $ Object $ config <> base
+
+base :: Object
+base =
+  KeyMap.fromList
+    [ "manifest_version" .= (3 :: Int),
+      "name" .= ("see" :: Text),
+      "version" .= ("0.1.0" :: Text)
+    ]
+
+firefox :: Object
+firefox =
+  KeyMap.fromList
+    [ "background"
+        .= object
+          [ "scripts" .= ["js/background.js" :: Text],
+            "type" .= ("module" :: Text)
+          ]
+    ]
+
+chrome :: Object
+chrome =
+  KeyMap.fromList []
