def6d65ae9826d83ed5bb82aada62493bfe10be3
a <a@as-Virtual-Machine.local>
2026-06-07 03:29:07 -0700

    Remove unused Firefox CSP preference overrides from development script

--- devenv.nix
+++ devenv.nix
@@ -29,14 +29,11 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  # https://github.com/mozilla-firefox/firefox/blob/a7628a666be5c4e35430780ff228dc185cbd8533/modules/libpref/init/all.js#L3157-L3158
-  # https://github.com/mozilla-firefox/firefox/blob/a7628a666be5c4e35430780ff228dc185cbd8533/modules/libpref/init/all.js#L3160
+  # https://github.com/mozilla-firefox/firefox/blob/a7628a666be5c4e35430780ff228dc185cbd8533/modules/libpref/init/all.js#L3158
   scripts.browse.exec = ''
     cd "$DEVENV_ROOT/cljs/public" && web-ext run --devtools \
     --pref devtools.toolbox.alwaysOnTop=false \
-    --pref extensions.webextensions.base-content-security-policy.v3="script-src 'self' 'wasm-unsafe-eval' 'unsafe-eval';" \
-    --pref extensions.webextensions.base-content-security-policy.v3-with-localhost="script-src 'self' 'wasm-unsafe-eval' http://localhost:* http://127.0.0.1:* 'unsafe-eval';" \
-    --pref extensions.webextensions.default-content-security-policy.v3="script-src 'self' 'unsafe-eval'; upgrade-insecure-requests;"
+    --pref extensions.webextensions.base-content-security-policy.v3-with-localhost="script-src 'self' 'wasm-unsafe-eval' http://localhost:* http://127.0.0.1:* 'unsafe-eval';"
   '';
   scripts.hello.exec = ''
     echo hello from $GREET
