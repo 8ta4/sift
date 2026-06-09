9ca69cad0fe115e0febbd71e56dd444d0361b579
a <a@as-Virtual-Machine.local>
2026-06-08 22:21:39 -0700

    Add function to compute socket path in temporary directory

--- hs/app/Host.hs
+++ hs/app/Host.hs
@@ -1,6 +1,13 @@
 module Host (main) where
 
 import Relude
+import System.Directory (getTemporaryDirectory)
+import System.FilePath ((</>))
 
 main :: IO ()
 main = pure ()
+
+getSocketPath :: IO FilePath
+getSocketPath = do
+  temporaryDirectory <- getTemporaryDirectory
+  pure $ temporaryDirectory </> "sift.sock"
