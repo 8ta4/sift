9de93d76dff7a2e8baca25d5da5ed767141ec056
a <a@as-Virtual-Machine.local>
2026-06-08 22:27:35 -0700

    Add safe file‑removal logic to delete existing socket before startup

--- hs/app/Host.hs
+++ hs/app/Host.hs
@@ -1,13 +1,25 @@
 module Host (main) where
 
+import Control.Exception (catch, throwIO)
 import Relude
-import System.Directory (getTemporaryDirectory)
+import System.Directory (getTemporaryDirectory, removeFile)
 import System.FilePath ((</>))
+import System.IO.Error (isDoesNotExistError)
 
 main :: IO ()
-main = pure ()
+main = do
+  socketPath <- getSocketPath
+  removeIfExists socketPath
 
 getSocketPath :: IO FilePath
 getSocketPath = do
   temporaryDirectory <- getTemporaryDirectory
   pure $ temporaryDirectory </> "sift.sock"
+
+-- https://stackoverflow.com/a/8502391
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where
+    handleExists e
+      | isDoesNotExistError e = pure ()
+      | otherwise = throwIO e
