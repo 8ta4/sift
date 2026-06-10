d93938acba3d8dd5ecbef48ce17d0fb63a86d673
a <a@as-Virtual-Machine.local>
2026-06-09 19:13:25 -0700

    Implement browse function to open formatted URL in new Chrome window

--- cljs/src/background.cljs
+++ cljs/src/background.cljs
@@ -1,10 +1,13 @@
-(ns background)
+(ns background
+  (:require
+   [goog.string :refer [format]]))
 
 (defonce port
   (js/chrome.runtime.connectNative "host"))
 
 (defn browse
-  [text reference])
+  [text reference]
+  (js/chrome.windows.create (clj->js {:url (format reference text)})))
 
 (defn handle-host
   [message]
