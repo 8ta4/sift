8055cd0612c8955246b28a43cc162cd4042ad720
a <a@as-Virtual-Machine.local>
2026-06-09 19:04:21 -0700

    Add browse function and integrate it into host message handling

--- cljs/src/background.cljs
+++ cljs/src/background.cljs
@@ -3,10 +3,15 @@
 (defonce port
   (js/chrome.runtime.connectNative "host"))
 
+(defn browse
+  [text reference])
+
 (defn handle-host
   [message]
   (js/console.log "Message from host:")
-  (js/console.log message))
+  (js/console.log message)
+  (run! (partial browse (:text (js->clj message :keywordize-keys true)))
+        (:references (js->clj message :keywordize-keys true))))
 
 (defn init
   []
