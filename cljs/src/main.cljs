0bc8bd72b3c549d1c2432cb97fe56132d9d8ada2
a <a@as-Virtual-Machine.local>
2026-06-18 01:24:12 -0700

    Enable winfixbuf for list and filter windows

--- cljs/src/main.cljs
+++ cljs/src/main.cljs
@@ -127,6 +127,7 @@
                         :silent true}))
             (cartesian-product modes (map name actions)))
       (.setOption list-buffer "buftype" "acwrite")
+      (run! #(.setOption % "winfixbuf" true) #{list-window filter-window})
       (let [items (read-string {:readers {'ordered/map ordered-map}} (slurp path))]
         (transform ATOM
                    (comp (partial setval* :window {:filter (.-id filter-window)
