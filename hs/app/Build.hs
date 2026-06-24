fb255cd847d8f70245523ebd34f023f8375528ef
a <a@as-Virtual-Machine.local>
2026-06-23 18:52:00 -0700

    Add icon definition to Chrome manifest

--- hs/app/Build.hs
+++ hs/app/Build.hs
@@ -33,13 +33,7 @@ firefox =
           [ "scripts" .= ["js/background.js" :: Text],
             "type" .= ("module" :: Text)
           ],
-      "browser_specific_settings"
-        .= object
-          [ "gecko"
-              .= object
-                [ "id" .= ("@sift" :: Text)
-                ]
-          ]
+      "browser_specific_settings" .= object ["gecko" .= object ["id" .= ("@sift" :: Text)]]
     ]
 
 chrome :: Object
@@ -50,5 +44,6 @@ chrome =
           [ "service_worker" .= ("js/background.js" :: Text),
             "type" .= ("module" :: Text)
           ],
+      "icons" .= object ["128" .= ("icon.png" :: Text)],
       "key" .= ("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsLBbTxtoxub0JD2snkyNBk87JLch7w8GGK5Wupi/P1Q3IS8qvMToUPFcRRLEUW6x+YrgcAtaAjuYjzr8Cye8yTEeLrwvtEqZ1+lH6XpmyZthB/4kBvvMC/rDEgJ4zu2MqjPNEGtcJnPwK5pkZGEzYXZgmAB0YFqGE3LWiGdtC+wShCp8hO4SPOyqytrwz95lM969cEhIV90x9CJHtINSFY2MBIs1GT1gvnnyYUZzhQTgretJZiNq4PdwUKY9pdzU1i2/DKWuhwnZx+D4iMNgNjx/gXU1rYrQ/H5afYA23vq7y6LwShOXRag0UGPqUZDg2ZHkdUQk2uwPXCCeN4QHSwIDAQAB" :: Text)
     ]
