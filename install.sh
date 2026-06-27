0340bdd34f74032e3af251e0f28e33d7563aac65
a <a@as-Virtual-Machine.local>
2026-06-27 02:20:23 -0700

    Bump sift version and update checksum

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -e
 FILE="plugin.tar.gz"
 trap 'rm -f "$FILE"' EXIT
-curl -L https://github.com/8ta4/sift/releases/download/v0.1.1/plugin.tar.gz -o $FILE
-echo "17f2452cd3c21ec51873a4cef480f172d9c853b1bd0d2c5ddb1b4f19debe8b43  $FILE" | shasum -a 256 -c
+curl -L https://github.com/8ta4/sift/releases/download/v0.1.2/plugin.tar.gz -o $FILE
+echo "7a602463c94466c851feab1c801e67752c607bc2e6fbf78d3da5d577bccf7c98  $FILE" | shasum -a 256 -c
 tar -xzf $FILE
